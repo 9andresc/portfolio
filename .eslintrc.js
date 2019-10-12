926fb27e3da842e3a32306573cd7e3182b679484
Gustavo <andrescabral.c@gmail.com>
2019-10-12 02:25:56 +0000

    refactor: change to TS

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,16 @@
 module.exports = {
-  "extends": ["prettier"],
-  "plugins": ["prettier"],
-  "rules": {
-    "prettier/prettier": ["warn"]
+  ecmaFeatures: {
+    jsx: true,
   },
-}
+  extends: ['plugin:react/recommended', 'plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
