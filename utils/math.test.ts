ddb02c69b4999bf3cebfe1adfa4bf005b26d06d9
Gustavo <andrescabral.c@gmail.com>
2019-10-13 19:04:02 +0000

    test(math): fix multiply test

--- utils/math.test.ts
+++ utils/math.test.ts
@@ -8,6 +8,6 @@ test('subtracts 3 - 1 and returns 2', () => {
   expect(sub(3, 1)).toBe(2);
 });
 
-test('multiplies 2 * 3 and returns 8', () => {
-  expect(multiply(2, 3)).toBe(8);
+test('multiplies 2 * 3 and returns 6', () => {
+  expect(multiply(2, 3)).toBe(6);
 });
